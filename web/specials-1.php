35de4ffc178e0d599ffe089257108aba4fcd5f09
TechCode Hive <81313404+tcHive@users.noreply.github.com>
2021-09-07 16:41:35 +0200

    array example

--- web/specials-1.php
+++ web/specials-1.php
@@ -1,1 +1,25 @@
 <?php
+
+    class product{
+        private $name;
+        private $price;
+
+        public function __construct($name, $price){
+
+            $this->name = $name;
+            $this->price = $price;
+        }
+
+        public function name(){
+            return $this->name;
+        }
+
+        public function price(){
+            return $this->price;
+        }
+    }
+
+    $values = [
+        new product('banana', 2.25),
+        new product('apple', 1.25)
+    ];
