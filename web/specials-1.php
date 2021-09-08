2416b8bcbb2a703c3dfe1b62ffc401d076b62e55
Tak-ie <zachariambulaheni@gmail.com>
2021-09-08 14:09:34 +0200

    bs

--- web/specials-1.php
+++ web/specials-1.php
@@ -7,7 +7,7 @@
         private $title;
         private $description;
 
-        public function __construct($name,$price, $image, $title, $description){
+        public function __construct($name, $image, $title,$price, $description){
 
             $this->name = $name;
             $this->title = $title;
@@ -38,7 +38,7 @@
     }
 
     $values = [
-        new product('V01','Cheese Vegitable','D6.jpg', 'R45.00','Brocolli, beans, and shredded cheese served in a crispy shell with your choice of sour cream and salsa accompanied by soup '),
+        new product('V01','Cheese Vegitable','D6', 'R45.00','Brocolli, beans, and shredded cheese served in a crispy shell with your choice of sour cream and salsa accompanied by soup '),
         new product('V02','Mixed Vegitables','Vg-1', 'R55.00','broccoli, carrots, baby corn, water chestnuts, mushrooms, and green beans sauteed in brown sauce')
     ];
     //echo $values=[product=>('v02')];
