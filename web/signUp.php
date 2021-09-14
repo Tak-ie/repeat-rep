9d1c8ec67311bdfbd4e4729dd0494d4514a3bb81
Tak-ie <zachariambulaheni@gmail.com>
2021-09-14 19:04:47 +0200

    dm

--- web/signUp.php
+++ web/signUp.php
@@ -2,17 +2,17 @@
 
 
 if(array_key_exists('subscribe',$_POST)){
-    $name = $_POST['FirstName'];
-    $lastName =$_POST['LastName'];
-    $email = $_POST['Email'];
+    $firstName = $_POST['firstName'];
+    $lastName =$_POST['lastName'];
+    $email = $_POST['email'];
     
 
     $error=[];
-    if(!trim($name)){
-        $error['name'] = 'first name cannot be empty';
+    if(!trim($firstName)){
+        $error['firstName'] = 'first name cannot be empty';
     }
     if(!trim($lastName)){
-        $error['lastname'] = 'last name cannot be empty';
+        $error['lastName'] = 'last name cannot be empty';
     }
     if(!trim($email)){
         $error['email'] = 'email cannot be empty';
@@ -23,49 +23,17 @@ if(array_key_exists('subscribe',$_POST)){
     if(empty($error)){
         //
 
-        $sub = new Subscriber();
-        $obj = $sub->insert('Rudzi','Ramuludzi','vhuthu@gmai.com','register_early.jpg'); 
+        $sub = new Subscriber('Rudzi','Ramuludzi','vhuthu@gmai.com');
+        $sub->insert($this->firstName,$this->lastName,$this->email); 
     }
 
 
 }
 
-class Display{
-    private $name;
-    private $title;
-    private $image;
-    private $message;
 
-    public function __construct($name,$title, $image,$message){
 
-        $this->name = $name;
-        $this->title = $title;
-        $this->image = $image;
-        $this->message =$message;
-    }
 
-    public function name(){
-        return $this->name;
-    }
-
-    public function title(){
-        return $this->title;
-    }
-
-    public function price(){
-        return $this->price;
-    }
-
-    public function image(){
-        return $this->image;
-    }
-    public function message(){
-        return $this->message;
-    }
-
-}
-
-$dsp =new Display('Subscribe for our news letter and products','Sign up','register_early.jpg','We voucher your privacy, no spam!');
+//$dsp =new Display('Subscribe for our news letter and products','Sign up','register_early.jpg','We voucher your privacy, no spam!');
 
 
      
